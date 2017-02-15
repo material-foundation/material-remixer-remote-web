8699c9e2651585f710308572139dab8681f3d5bc
Chris Cox <cjcox@google.com>
2017-02-15 15:09:49 -0800

    Renames styles.

--- copy-src.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-copyFiles -f \
-  node_modules/material-design-lite/material.min.{css,js,*.map} \
-  node_modules/material-remixer/dist/remixer.min.{js,*.map} \
-  src/* \
-  public
-
-echo "=== Source files have been copied to: ./public ==="
